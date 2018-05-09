7ca2f16af9ee7f411744407e774ee6adf1bde1e0
Grady_Liu <grady.liu@ischool.com.tw>
2018-05-09 11:58:42 +0800

    2018/05/09_羿均_修改權限代碼_避免更幹部資料權限代碼重複。

--- AllWorkReport/Permissions.cs
+++ AllWorkReport/Permissions.cs
@@ -8,16 +8,16 @@ namespace AllWorkReport
 {
      public static class Permissions
      {
+        public static string 轉學報表列印 { get { return "EC924F9D-B278-42F2-A6D3-608816272478"; } }
+        //public static string 轉學報表列印 { get { return "K12.Student.TheCadre.Detail00040"; } }
 
-          public static string 轉學報表列印 { get { return "K12.Student.TheCadre.Detail00040"; } }
-
-          public static bool 轉學報表列印權限
-          {
-               get
-               {
-                    bool check1 = FISCA.Permission.UserAcl.Current[轉學報表列印].Executable;
-                    return check1;
-               }
-          }
+        public static bool 轉學報表列印權限
+        {
+            get
+            {
+                bool check1 = FISCA.Permission.UserAcl.Current[轉學報表列印].Executable;
+                return check1;
+            }
+        }
      }
 }
